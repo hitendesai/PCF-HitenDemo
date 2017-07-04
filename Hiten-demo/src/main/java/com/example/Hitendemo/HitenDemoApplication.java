60119abd9c6944125169728d98f2a057e7ac75b5
Hiten Desai <hiten.sai@gmail.com>
2017-07-04 15:05:54 -0700

    First Commit to this demp application

---
+++ Hiten-demo/src/main/java/com/example/Hitendemo/HitenDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.Hitendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitenDemoApplication.class, args);
+	}
+}
